edad14bf663f5999e9c14c407e903aacc6deef1a
dv <acnt.dv@gmail.com>
2025-05-02 19:13:57 +0330

    minor improvements in mobile view

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import image from '../statics/png/main-title.png';
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MiniSlideShow from "./MiniSlideShow";
 import FadingTextBoxWithMask from "./FadingTextBoxWithMask";
 
@@ -20,54 +20,54 @@ into timeless masterpieces. With a blend of cutting-edge technology and sustaina
 
     useEffect(() => {
         let maxTranslate = 0;
-            if (window.innerWidth <= 768) {
-                // 'md' breakpoint in Tailwind (768px)
-                maxTranslate=(window.innerHeight * 0.47);
-            } else {
-                maxTranslate=(window.innerHeight * 0.75);
+        if (window.innerWidth <= 768) {
+            // 'md' breakpoint in Tailwind (768px)
+            maxTranslate = (window.innerHeight * 0.47);
+        } else {
+            maxTranslate = (window.innerHeight * 0.75);
+        }
+
+        const handleScroll = () => {
+            // Get the current scroll position
+            const position = window.scrollY;
+            let maxLimit = maxTranslate;
+
+            if (miniSlideRef.current) {
+                const slideShowRect = miniSlideRef.current.getBoundingClientRect();
+                const bottomLimit = slideShowRect.top + slideShowRect.height - 150; // Adjust offset as needed
+
+                if (position >= bottomLimit) {
+                    maxLimit = bottomLimit;
+                }
             }
 
-            const handleScroll = () => {
-                // Get the current scroll position
-                const position = window.scrollY;
-                let maxLimit = maxTranslate;
+            setTranslateY(Math.min(position, maxLimit));
+        };
 
-                if (miniSlideRef.current) {
-                    const slideShowRect = miniSlideRef.current.getBoundingClientRect();
-                    const bottomLimit = slideShowRect.top + slideShowRect.height - 150; // Adjust offset as needed
-
-                    if (position >= bottomLimit) {
-                        maxLimit = bottomLimit;
-                    }
-                }
-
-                setTranslateY(Math.min(position, maxLimit));
-            };
-
-            window.addEventListener("scroll", handleScroll);
-            return () => window.removeEventListener("scroll", handleScroll);
-        }, []
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []
     );
 
     return (
         <div className="flex flex-col w-full bg-foko">
             <div
                 className="min-h-[75vh] w-full bg-cover bg-center bg-gray-400"
-                style={{backgroundImage: `url(${image})`}}>
+                style={{ backgroundImage: `url(${image})` }}>
             </div>
-            <div className="flex w-full items-start justify-start sm:justify-center text-black">
-                <div className="flex justify- items-center mt-[30px] md:mt-[50px]">
-                    <p className="mt-[30px] md:mt-[160px] mx-[30px] md:mx-0 md:absolute md:left-[50px] font-rubik text-10 md:text-30 text-end break-words -translate-y-1/2 text-gray-900 transition-transform duration-300"
-                    //    style={{transform: `translateY(${translateY}px)`,}}
-                       >BEL AIR <br/> RESIDENTIAL
+            <div className="flex w-full mx-0 items-start justify-center md:justify-start text-black">
+                <div className="flex items-start mt-[30px] md:mt-[50px] md:w-1/5">
+                    {/* <p className="mt-[30px] md:mt-[160px] mx-[30px] md:mx-0 md:absolute md:left-[50px] font-rubik text-10 md:text-30 text-end break-words -translate-y-1/2 text-gray-900 transition-transform duration-300" style={{transform: `translateY(${translateY}px)`,}}*/}
+                    <p className='mx-[30px] md:mx-foko font-rubik text-16 md:text-30 text-end break-words mt-[-4px] md:mt-[-10px]'>
+                        BEL AIR <br /> RESIDENTIAL
                     </p>
                 </div>
                 <div className="flex-col w-3/5 space-y-16 items-center justify-end mr-[30px] md:mr-0 text-black mb-[150px]">
                     {/* <FadingTextBoxWithMask text={textLines}/> */}
-                    <p className='text-justify mt-[30px] md:mt-[50px]'>
+                    <p className='text-justify mt-[30px] md:mt-[50px] text-10 md:text-16'>
                         {textLines}
                     </p>
-                    <MiniSlideShow/>
+                    <MiniSlideShow />
                 </div>
             </div>
         </div>
