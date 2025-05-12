d2287fbc367e02fa3c66cc8ba3f1a782fef01370
dv <acnt.dv@gmail.com>
2025-05-12 15:44:23 +0330

    studio and profile improved

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import profileImage from "../statics/png/about-us.png"; // Replace with actual image path
 
-const ProfileCard = ({name}) => {
+const ProfileCard = ({ name }) => {
     return (
-        <div className="flex-col bg-foko min-h-screen justify-start w-[calc(100%-60px)] md:w-3/4">
+        <div className="flex-col bg-foko min-h-full justify-start w-[calc(100%-60px)] md:w-3/4 mt-[125px] ml-[125px]">
             <div className="flex flex-col md:flex-row w-full">
                 <div className="relative w-full h-full aspect-[1/1.5] md:w-1/3 md:max-w-[528px] md:max-h-[698px] p-0 flex flex-col items-center">
                     {/* Profile Image */}
@@ -14,7 +14,7 @@ const ProfileCard = ({name}) => {
                     />
                 </div>
 
-                <div className="w-full md:w-2/3 px-0 my-[30px] md:my-0 md:pl-[50px] flex-col justify-items-start">
+                <div className="relative w-full md:w-2/3 px-0 my-[30px] md:my-0 md:pl-[50px] flex-col justify-items-start">
                     {/* Name and Title */}
                     <h1 className="font-rubik text-16 md:text-30 font-bold">{name}</h1>
                     <h2 className="font-rubik text-16 md:text-30 font-light uppercase mt-2">Co-Founder</h2>
@@ -49,16 +49,29 @@ const ProfileCard = ({name}) => {
                         collaboration at all
                         scales.
                     </p>
+                    <div className='absolute left-[15px] bottom-[-10px]'>
+                        <div className="flex flex-row justify-between items-end w-full px-6">
+                            {/* جدول مشخصات پروژه */}
+                            <div className="text-left mb-[10px] mr-[40px] text-[12px] leading-[1.8]">
+                                <div className="grid grid-cols-2 gap-x-4 gap-y-1 p-2 w-full">
+                                    <span className="font-bold">Masters of Architecture ||</span>
+                                    <span className="text-right">Yale University</span>
+                                    <span className="font-bold">Bachelor of Architecture</span>
+                                    <span className="text-right">Pratt Institute</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="mt-0 md:mt-[50px] relative">
+            {/* <div className="mt-0 md:mt-[50px] relative">
 
                 <h3 className="text-start font-rubik text-16 md:text-30 font-semibold uppercase mb-[30px] md:my-[50px]">Education:</h3>
                 <p className="text-start font-rubik text-gray-800 text-10 md:text-16 mt-1">Masters of Architecture</p>
                 <p className="text-start font-rubik text-gray-800 text-10 md:text-16">Yale University</p>
                 <p className="text-start font-rubik text-gray-800 text-10 md:text-16 mt-2">Masters of Architecture</p>
                 <p className="text-start font-rubik text-gray-800 text-10 md:text-16 mb-[30px]">Yale University</p>
-            </div>
+            </div> */}
         </div>
     );
 };
--- src/components/Studio.jsx
+++ src/components/Studio.jsx
@@ -4,55 +4,74 @@ import ProfileCard from "./ProfileCard";
 const AboutUs = () => {
     return (
         <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px]">
-            <div className="flex flex-col-reverse md:flex-row w-full">
+            <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px] box-border px-[100px]">
+                <div className="flex flex-col-reverse md:flex-row w-full">
+                    
+                    {/* متن سمت چپ */}
+                    <div className="flex flex-col w-full md:w-1/2 justify-between">
+                        <div className='flex w-full '>
+                            <div className='pr-[50px] min-w-[250px]'>
+                                <h1 className="text-right text-10 md:text-30">
+                                    About <br /> FOKO
+                                </h1>
+                            </div>
+                            <div className='pr-[200px]'>
+                                <p className="text-10 md:text-16 text-justify">
+                                    At FOKO Studio, we are deeply committed to our clients' specific needs. From intimate interiors to large scale architectural projects, we combine creative problem-solving with a holistic design approach, navigating the layered complexities of each project. By considering every detail, we design environments that are perfectly tailored to the unique lifestyle and aspirations of our clients.
+                                </p>
+                            </div>
+                        </div>
 
-                <div className="flex-col w-full md:w-1/2 h-full items-center justify-center">
-                    <div className='flex'>
-                        <h1 className="text-16 md:text-30 font-rubik block absolute left-[30px] text-right mt-[25px] md:mt-[40px]">About <br /> FOKO
-                        </h1>
-                        <span className="flex flex-col w-full justify-center items-center text-justify my-[30px] md:my-foko">
-                            <p className="mx-[30px] md:mx-[50px] text-10 md:text-16 w-1/2 self-end md:self-center ">
-                                FOKO Studio is a Los Angeles-based architecture and interior design firm.
-                                Our studio specializes in crafting bespoke, high-end residential and commercial
-                                spaces that are as thoughtful as they are visually compelling.
-                            </p>
+                        {/* OUR PHILOSOPHY */}
 
-                            <p className="mx-[30px] md:mx-[50px] w-1/2 self-end md:self-center text-10 md:text-16 my-foko">
-                                At FOKO Studio, we are deeply committed to our clients' specific needs.
-                                From intimate interiors to large-scale architectural projects,
-                                we combine creative problem-solving with a holistic design approach,
-                                navigating the layered complexities of each project. By considering every detail,
-                                we design environments that are perfectly tailored to the unique lifestyle and
-                                aspirations of our clients.
-                            </p>
-                        </span>
+                        <div className='flex w-full '>
+                            <div className='pr-[50px] min-w-[250px]'>
+                                <h1 className="text-right text-16 md:text-30">
+                                    OUR <br /> PHILOSOPHY
+                                </h1>
+                            </div>
+                            <div className='pr-[200px]'>
+                                <p className="text-10 md:text-16 text-justify">
+                                    At the heart of our practice is the belief that design should create meaningful spaces that enrich the lives of those who inhabit them. We combine critical thinking, technical skill, and a deep respect for context to transform challenges into opportunities and ideas into reality. Whether addressing spatial constraints, aesthetic aspirations, or functional needs, our creative solutions aim to enhance both the experience and impact of each space.
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex'>
-                        <h1 className="text-16 md:text-30 font-rubik block absolute left-[30px] text-right mt-[47px] md:mt-[40px]">OUR <br /> PHILOSOPHY
-                        </h1>
-                        <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
-                            <p className="mx-[30px] md:mx-[50px] text-10 md:text-16 w-1/2 self-end md:self-center">
-                                At the heart of our practice is the belief that design should create meaningful spaces that enrich the lives of those who inhabit them. We combine critical thinking, technical skill, and a deep respect for context to transform challenges into opportunities and ideas into reality. Whether addressing spatial constraints, aesthetic aspirations, or functional needs, our creative solutions aim to enhance both the experience and impact of each space.
-                            </p>
-                        </span>
+
+                    {/* تصویر سمت راست */}
+                    <div className="flex w-full md:w-1/2">
+                        <img
+                            src={aboutUsImage}
+                            alt="aboutUsImage"
+                            className="w-full h-auto object-cover"
+                        />
                     </div>
                 </div>
-                <div className="flex w-full md:w-1/2">
-                    <img src={aboutUsImage} alt={'aboutUsImage'} className="w-fit h-fit mb-0 md:mb-foko md:m-foko" />
+
+                <div className="flex flex-col-reverse md:flex-row w-full pt-[200px]">
+                    <div className='flex w-full '>
+                        <div className="flex w-full md:w-1/2">
+
+                            <div className='pr-[50px] min-w-[250px]'>
+                                <h1 className="text-right text-16 md:text-30">
+                                    THE <br /> FOUNDERS
+
+                                </h1>
+                            </div>
+                            <div className='pr-[200px]'>
+                                <p className="text-10 md:text-16 text-justify">
+                                    FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design professionals with a shared passion for creating inspiring spaces. With diverse backgrounds in architecture and interior design, Jenny and Pegah bring a unique blend of technical expertise and creative intuition to every project. Their collaboration fosters a dynamic, multidisciplinary approach that excels across projects of all scales.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className='flex w-full mb-[30px] md:mb-[50px]'>
-                <h1 className="text-16 md:text-30 font-rubik block absolute left-[30px] text-right mt-[47px] md:mt-[40px]">OUR <br/> TEAM</h1>
-                <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
-                    <p className="mx-[30px] md:mx-[50px] text-10 md:text-16 w-1/2 md:w-9/12 self-end md:self-center">
-                        FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design professionals with a shared passion for creating inspiring spaces. With diverse backgrounds in architecture and interior design, Jenny and Pegah bring a unique blend of technical expertise and creative intuition to every project. Their collaboration fosters a dynamic, multidisciplinary approach that excels across projects of all scales.
-                    </p>
-                </span>
+
+                <ProfileCard name='JENNY FONTENOT, RA'/>
+                <ProfileCard name='PEGAH KOULAEIAN, LEED AP' />
+                <span className="mb-[10px] md:mb-[200px]" />
             </div>
 
-            <ProfileCard name='JENNY FONTENOT, RA' />
-            <ProfileCard name='PEGAH KOULAEIAN, LEED AP' />
-            <span className="mb-[50px] md:mb-[50px]" />
         </div>
     )
 }
