ebe8f3cee7461f31f3edc80cc7095334052639b5
dv <acnt.dv@gmail.com>
2025-05-12 11:46:22 +0330

    works handled

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,14 +9,8 @@ export const Works = () => {
     const miniSlideRef = useRef(null);
 
     const textLines =
-        `Welcome to FOKO STUDIO , where innovation meets tradition in the world of architecture. Our team of passionate architects and designers is 
-dedicated to transforming spaces into timeless masterpieces. With a blend of cutting-edge technology and sustainable practices, we create 
-environments that inspire and endure. At [Your Firm’s Name], we believe in the power of collaboration and creativity. Our projects... Welcome to 
-FOKO STUDIO , where innovation meets tradition in the world of architecture. Our team of passionate architects and designers is dedicated to 
-transforming spaces into timeless masterpieces. With a blend of cutting-edge technology and sustainable practices, we create environments that 
-inspire and endure. At [Your Firm’s Name], we believe in the power of collaboration and creativity. Our projects...dedicated to transforming spaces
-into timeless masterpieces. With a blend of cutting-edge technology and sustainable practices, we create environments that inspire and endure. At 
-[Your Firm’s Name], we believe in the power of collaboration and creativity. Our project`
+        `This 11 bedroom private home sits on a Hillside site in Bel Air with expansive views of Los Angeles. The contemporary style of the home incorporates subtle nods to the client’s Japanese roots through the use of natural materials that blurred the line between landscape and architecture. This projectwas unbuilt due to economic strains from the pandemic.
+`
 
     useEffect(() => {
         let maxTranslate = 0;
@@ -55,18 +49,37 @@ into timeless masterpieces. With a blend of cutting-edge technology and sustaina
                 className="min-h-[75vh] w-full bg-cover bg-center bg-gray-400"
                 style={{ backgroundImage: `url(${image})` }}>
             </div>
-            <div className="flex w-full mx-0 items-start justify-center md:justify-start text-black">
-                <div className="flex items-start mt-[30px] md:mt-[50px] md:w-1/5">
-                    {/* <p className="mt-[30px] md:mt-[160px] mx-[30px] md:mx-0 md:absolute md:left-[50px] font-rubik text-10 md:text-30 text-end break-words -translate-y-1/2 text-gray-900 transition-transform duration-300" style={{transform: `translateY(${translateY}px)`,}}*/}
-                    <p className='mx-[30px] md:mx-foko font-rubik text-16 md:text-30 text-end break-words mt-[-4px] md:mt-[-10px]'>
+            <div className="flex w-full mx-0 mt-[200px] items-start justify-center md:justify-center text-black">
+                {/* <div className="flex items-center mt-[30px] md:mt-[50px] md:w-1/5">
+                    <p className="mt-[30px] md:mt-[160px] mx-[30px] md:mx-0 md:absolute md:left-[50px] font-rubik text-10 md:text-30 text-end break-words -translate-y-1/2 text-gray-900 transition-transform duration-300" style={{transform: `translateY(${translateY}px)`,}}></p>
+                    
+                </div> */}
+                <div className="relative flex-col w-3/5 space-y-16 items-center justify-end mr-[30px] md:mr-0 text-black mb-[150px]">
+                    <p className='absolute left-[-275px] top-[50px] font-rubik text-16 md:text-30 text-end break-words md:mt-[10px]'>
                         BEL AIR <br /> RESIDENTIAL
                     </p>
-                </div>
-                <div className="flex-col w-3/5 space-y-16 items-center justify-end mr-[30px] md:mr-0 text-black mb-[150px]">
-                    {/* <FadingTextBoxWithMask text={textLines}/> */}
-                    <p className='text-justify mt-[30px] md:mt-[50px] text-10 md:text-16'>
+                    <p className='text-justify mt-[60px] w-full text-10 md:text-16'>
                         {textLines}
                     </p>
+                    <div className='absolute left-[-350px] bottom-[-90px]'>
+                        <div className="flex flex-row justify-between items-end w-full px-6">
+                            {/* جدول مشخصات پروژه */}
+                            <div className="text-left mb-[10px] mr-[40px] text-[12px] leading-[1.8]">
+                                <div className="grid grid-cols-[80px_auto] gap-x-4 gap-y-1 p-2 w-fit">
+                                    <span className="font-bold">PROGRAM</span>
+                                    <span className="text-right">Single Family Residential</span>
+                                    <span className="font-bold">LOCATION</span>
+                                    <span className="text-right">Bel Air</span>
+                                    <span className="font-bold">SIZE</span>
+                                    <span className="text-right">24,000 Sf</span>
+                                    <span className="font-bold">TYPE</span>
+                                    <span className="text-right">New Construction</span>
+                                    <span className="font-bold">STATUS</span>
+                                    <span className="text-right">Unbuilt</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <MiniSlideShow />
                 </div>
             </div>
