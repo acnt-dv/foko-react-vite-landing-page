8c5155e2a263ab0c35d31308669048b4b6f396ec
dv <acnt.dv@gmail.com>
2025-05-14 13:52:27 +0330

    mobile view improved

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -4,62 +4,62 @@ import profileImage from "../statics/png/about-us.png"; // Replace with actual i
 const ProfileCard = ({ name }) => {
     return (
         // <div className="flex-col bg-foko min-h-full justify-start w-[calc(100%-60px)] md:w-full px-[100px] mt-[125px]">
-        <div className="flex bg-foko min-h-full md:w-full mt-[125px] justify-end">
+        <div className="flex bg-foko min-h-full md:w-full mt-[100px] md:mt-[200px] justify-center md:justify-end">
             <div className="flex flex-col md:flex-row w-10/12">
                 <div className="relative w-full h-full aspect-[1/1.5] md:w-1/3 md:max-w-[528px] md:max-h-[698px] flex flex-col items-center">
                     {/* Profile Image */}
                     <img
                         src={profileImage}
                         alt="Jenny Fontenot"
-                        className="absolute ml-[-25px] top-0 left-0 w-full h-full object-cover shadow-lg"
+                        className="absolute m-0 md:ml-[-25px] top-0 left-0 w-full h-full object-cover shadow-lg"
                     />
                 </div>
 
-                <div className="relative w-full md:w-2/3 px-0 my-[30px] md:my-0 md:pl-[50px] flex-col justify-items-start">
-                    {/* Name and Title */}
-                    <h1 className="font-rubik text-16 md:text-30 font-bold">{name}</h1>
-                    <h2 className="font-rubik text-16 md:text-30 font-light uppercase mt-2">Co-Founder</h2>
+                <div className="relative w-full md:w-2/3 px-0 my-[30px] md:my-0 md:pl-[50px] flex flex-col justify-between">
+                    <div>
+                        {/* Name and Title */}
+                        <h1 className="font-rubik text-16 md:text-30 font-bold">{name}</h1>
+                        <h2 className="font-rubik text-16 md:text-30 font-light uppercase mt-2">Co-Founder</h2>
 
-                    {/* Description */}
-                    <p className="mt-4 text-justify text-10 md:text-16 leading-6">
-                        Jenny is the co-founder and Architectural Director of FOKO Studio. She is a licensed architect
-                        with a passion
-                        for creating positive impact for individuals and communities through shaping the built
-                        environment. With over
-                        10 years of experience in various typologies and scales, including retail, institutional,
-                        residential,
-                        workplace, and adaptive-reuse projects, Jenny has developed a diverse portfolio that showcases
-                        her versatility
-                        and expertise.
-                    </p>
+                        {/* Description */}
+                        <p className="mt-4 text-justify text-10 md:text-16 leading-6">
+                            Jenny is the co-founder and Architectural Director of FOKO Studio. She is a licensed architect
+                            with a passion
+                            for creating positive impact for individuals and communities through shaping the built
+                            environment. With over
+                            10 years of experience in various typologies and scales, including retail, institutional,
+                            residential,
+                            workplace, and adaptive-reuse projects, Jenny has developed a diverse portfolio that showcases
+                            her versatility
+                            and expertise.
+                        </p>
 
-                    <p className="mt-4 text-justify text-10 md:text-16 leading-6">
-                        As a licensed architect in both New York and California, Jenny brings a wealth of knowledge and
-                        understanding
-                        of different building regulations, allowing her to navigate complex projects while seeking
-                        innovative design
-                        solutions. Jenny sees the value in every detail and approaches each design challenge with an
-                        enthusiastic
-                        mindset, eager to unravel its puzzle-like nature.
-                    </p>
+                        <p className="mt-4 text-justify text-10 md:text-16 leading-6">
+                            As a licensed architect in both New York and California, Jenny brings a wealth of knowledge and
+                            understanding
+                            of different building regulations, allowing her to navigate complex projects while seeking
+                            innovative design
+                            solutions. Jenny sees the value in every detail and approaches each design challenge with an
+                            enthusiastic
+                            mindset, eager to unravel its puzzle-like nature.
+                        </p>
 
-                    <p className="mt-4 text-justify text-gray-800 text-10 md:text-16 leading-6">
-                        Extending beyond traditional architecture, Jenny’s interests include furniture and product
-                        design. She
-                        believes in creating holistic and captivating experiences for users through design and
-                        collaboration at all
-                        scales.
-                    </p>
-                    <div className='absolute left-[15px] bottom-[-10px]'>
-                        <div className="flex flex-row justify-between items-end w-full px-6">
-                            {/* جدول مشخصات پروژه */}
-                            <div className="text-left mb-[10px] mr-[40px] text-[12px] leading-[1.8]">
-                                <div className="grid grid-cols-2 gap-x-4 gap-y-1 p-2 w-full">
-                                    <span className="font-bold">Masters of Architecture ||</span>
-                                    <span className="text-right">Yale University</span>
-                                    <span className="font-bold">Bachelor of Architecture</span>
-                                    <span className="text-right">Pratt Institute</span>
-                                </div>
+                        <p className="mt-4 text-justify text-gray-800 text-10 md:text-16 leading-6">
+                            Extending beyond traditional architecture, Jenny’s interests include furniture and product
+                            design. She
+                            believes in creating holistic and captivating experiences for users through design and
+                            collaboration at all
+                            scales.
+                        </p>
+                    </div>
+                    <div className="flex flex-row justify-start items-end w-full">
+                        {/* جدول مشخصات پروژه */}
+                        <div className="text-left mt-[30px] md:mt-0 mb-[10px] mr-[40px] text-[10px] md:text-[12px] leading-[1.8]">
+                            <div className="grid grid-cols-2 gap-x-4 gap-y-1 w-full">
+                                <span className="font-bold">Masters of Architecture ||</span>
+                                <span className="text-right">Yale University</span>
+                                <span className="font-bold">Bachelor of Architecture</span>
+                                <span className="text-right">Pratt Institute</span>
                             </div>
                         </div>
                     </div>
--- src/components/Studio.jsx
+++ src/components/Studio.jsx
@@ -4,18 +4,18 @@ import ProfileCard from "./ProfileCard";
 const AboutUs = () => {
     return (
         <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px]">
-            <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px] box-border px-[100px]">
+            <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px] box-border px-0 md:px-[100px]">
                 <div className="flex flex-col-reverse md:flex-row w-full">
                     
                     {/* ABOUT US */}
                     <div className="flex flex-col w-full md:w-3/5 justify-between">
-                        <div className='flex w-full'>
+                        <div className='flex w-full mt-[50px] md:mt-0'>
                             <div className='w-1/5'>
                                 <h1 className="text-right text-10 md:text-30">
                                     ABOUT <br /> US
                                 </h1>
                             </div>
-                            <div className='w-4/5 pl-[50px] pr-[150px]'>
+                            <div className='w-4/5 px-[30px] md:pl-[50px] md:pr-[150px]'>
                                 <p className="text-10 md:text-16 text-justify">
                                 FOKO Studio is a Los Angeles-based architecture and interior design firm. Our studio specializes in crafting bespoke, high-end residential and commercial spaces that are as thoughtful as they are visually compelling.
                                 At FOKO Studio, we are deeply committed to our clients' specific needs. From intimate interiors to large scale architectural projects, we combine creative problem-solving with a holistic design approach, navigating the layered complexities of each project. By considering every detail, we design environments that are perfectly tailored to the unique lifestyle and aspirations of our clients.
@@ -25,13 +25,13 @@ const AboutUs = () => {
 
                         {/* OUR PHILOSOPHY */}
 
-                        <div className='flex w-full '>
+                        <div className='flex w-full mt-[50px] md:mt-0'>
                             <div className='w-1/5'>
-                                <h1 className="text-right text-16 md:text-30">
+                                <h1 className="text-right text-10 md:text-30">
                                     OUR <br /> PHILOSOPHY
                                 </h1>
                             </div>
-                            <div className='w-4/5 pl-[50px] pr-[150px]'>
+                            <div className='w-4/5 px-[30px] md:pl-[50px] md:pr-[150px]'>
                                 <p className="text-10 md:text-16 text-justify">
                                 At the heart of our practice is the belief that design should create meaningful spaces that enrich the lives of those who inhabit them. We combine critical thinking, technical skill, and a deep respect for context to transform challenges into opportunities and ideas into reality. Whether addressing spatial constraints, aesthetic aspirations, or functional needs, our creative solutions aim to enhance both the experience and impact of each space.
                                 </p>
@@ -49,17 +49,16 @@ const AboutUs = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col-reverse md:flex-row w-full pt-[200px]">
+                <div className="flex flex-col-reverse md:flex-row w-full pt-[50px] md:pt-[200px]">
                     <div className='flex w-full '>
                         <div className="flex w-full md:w-3/5">
 
                             <div className='w-1/5'>
-                                <h1 className="text-right text-16 md:text-30">
+                                <h1 className="text-right text-10 md:text-30">
                                     THE <br /> FOUNDERS
-
                                 </h1>
                             </div>
-                            <div className='w-4/5 pl-[50px] pr-[150px]'>
+                            <div className='w-4/5 px-[30px] md:pl-[50px] md:pr-[150px]'>
                                 <p className="text-10 md:text-16 text-justify">
                                 FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design professionals with a shared passion for creating inspiring spaces. With diverse backgrounds in architecture and interior design, Jenny and Pegah bring a unique blend of technical expertise and creative intuition to every project. Their collaboration fosters a dynamic, multidisciplinary approach that excels across projects of all scales.
                                 </p>
@@ -70,7 +69,7 @@ const AboutUs = () => {
 
                 <ProfileCard name='JENNY FONTENOT, RA'/>
                 <ProfileCard name='PEGAH KOULAEIAN, LEED AP' />
-                <span className="mb-[10px] md:mb-[200px]" />
+                <span className="mb-[200px]" />
             </div>
 
         </div>
