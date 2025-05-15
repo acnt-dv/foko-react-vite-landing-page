c907f1784f2624ca15b322eba554c325c6dd4a31
sajedi.dv <d.sajedi@fanap.ir>
2025-05-16 01:30:31 +0330

    hot fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,7 @@ export const Login = ({loggedIn, setLoggedIn})=> {
             <div className="w-[75px] h-[75px] lg:w-[130px] lg:h-[130px] content justify-center items-center">
                 <img src={logo} alt={'logo'} />
             </div>
-        <div className="absolute top-[calc(50%-50px)] lg:top-[calc(50%-0px)]">
+        <div className="absolute top-[calc(50%-25px)] lg:top-[calc(50%-0px)]">
             <button className="text-[12px] lg:text-20 font-rubik mt-[128px] cursor-pointer"
             onClick={()=>setLoggedIn(!loggedIn)}
             >PASSWORD &#62;</button>
