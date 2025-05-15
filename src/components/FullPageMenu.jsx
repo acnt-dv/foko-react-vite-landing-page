0e305c1b16da012d86e75effb213a70e29e36075
sajedi.dv <d.sajedi@fanap.ir>
2025-05-15 22:36:36 +0330

    logos size fixed

--- src/components/FullPageMenu.jsx
+++ src/components/FullPageMenu.jsx
@@ -9,13 +9,13 @@ const FullPageMenu = ({isOpen, onClose}) => {
     return (
         <div
             className={`z-10 fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
-            <button onClick={onClose} className="absolute top-[52.5px] right-[30px] md:top-[72.5px] md:right-[50px] cursor-pointer">
+            <button onClick={onClose} className="absolute top-[37.5px] right-[30px] md:top-[72.5px] md:right-[50px] cursor-pointer">
                 <img src={close} alt="close" width='30px' height='30px'/>
             </button>
 
             <div className="flex flex-col md:flex-row w-full items-center justify-center">
                 <span className='flex w-1/2 justify-center items-center m-[50px]'>
-                    <img src={logo} alt={'logo'} className='w-[130px] h-[130px] filter brightness-0 invert'/>
+                    <img src={logo} alt={'logo'} className='w-[75px] h-[75px] md:w-[130px] md:h-[130px] filter brightness-0 invert'/>
                 </span>
 
                 <nav>
