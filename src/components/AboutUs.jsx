8f2bc67b676a35862098b7af647300657bb8ad81
dv <acnt.dv@gmail.com>
2025-05-17 10:49:42 +0330

    minor improvements in ipad air size

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,12 +11,12 @@ const AboutUs = () => {
                     {/* <h1 className="text-end text-16 font-rubik block lg:hidden absolute left-[30px] mt-[48px]">About <br /> FOKO</h1> */}
                 </span>
                 <span className="flex justify-center lg:justify-start w-full lg:w-4/5 px-[30px] lg:px-0">
-                    <div className="flex flex-col justify-between items-between text-justify my-0 lg:my-0">
-                        <p className="md:mr-[50px] lg:mr-[100px] xl:mr-[150px] text-10 lg:text-16 max-w-[450px]">
+                    <div className="flex w-full flex-col justify-between items-between text-justify my-0 lg:my-0">
+                        <p className="lg:mr-[100px] xl:mr-[150px] text-10 lg:text-16 lg:max-w-[450px]">
                             Welcome to FOKO Studio, an architecture and interior design practice specializing in creating functional, inspiring spaces that reflect your unique vision. From residential to commercial projects, our team is dedicated to delivering exceptional design solutions.
                         </p>
 
-                        <p className="mx-[30px] lg:mr-[150px] hidden lg:block self-end lg:self-center text-16 max-w-[450px] my-foko">
+                        <p className="mx-[30px] lg:mr-[150px] hidden lg:block self-end lg:self-center text-16 w-full my-foko">
                             {/* At FOKO Studio, we are deeply committed to our clients' specific needs.
                         From intimate interiors to large-scale architectural projects,
                         we combine creative problem-solving with a holistic design approach,
@@ -24,11 +24,11 @@ const AboutUs = () => {
                         we design environments that are perfectly tailored to the unique lifestyle and
                         aspirations of our clients. */}
                         </p>
-                        <h2 className="hidden lg:flex font-rubik max-w-[450px] text-10 lg:text-20 cursor-pointer my-[50px] lg:my-0">
+                        <h2 className="hidden lg:flex font-rubik lg:max-w-[450px] text-10 lg:text-20 cursor-pointer my-[50px] lg:my-0">
                             MORE &nbsp; &nbsp; &nbsp;
                             <img src={right} alt="Right Image" />
                         </h2>
-                        <h2 className="block lg:hidden flex font-rubik max-w-[450px] text-10 lg:text-20 cursor-pointer my-[50px] lg:my-0">
+                        <h2 className="block lg:hidden flex font-rubik lg:max-w-[450px] text-10 lg:text-20 cursor-pointer my-[50px] lg:my-0">
                             MORE &nbsp; &nbsp; &nbsp;
                             <img src={rightSmall} alt="Right Image" width="9px" height="4px" className='mt-[-2px]'/>
                         </h2>
--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -8,7 +8,7 @@ const ContactUs = () => {
                 <div className='flex w-full text-justify justify-center lg:justify-start items-start'>
                     <h1 className="text-16 font-rubik text-end block lg:hidden mx-[30px]">Contact <br /> Us</h1>
                     <div className="col">
-                        <p className="mr-[30px] lg:mr-[150px] max-w-[450px] lg:self-center text-10 lg:text-16 mt-[4px] lg:mt-0">
+                        <p className="mr-[30px] lg:mr-[150px] lg:max-w-[450px] lg:self-center text-10 lg:text-16 mt-[4px] lg:mt-0">
                         Whether you're looking to start a new project, have questions about our services, or just want to say hello, our team is here to assist. Please reach out to us directly using the information below, and we’ll get back to you as soon as possible.
                         </p>
                         <p className="mt-[50px] lg:mt-[100px] text-10 lg:text-16">Pegah Koulaeian</p>
