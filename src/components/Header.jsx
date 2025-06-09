8716b47f96110603521177c3af0b389c03a4c466
dv <acnt.dv@gmail.com>
2025-06-09 15:25:44 +0330

    handle logo clicked navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import menuIcon from '../statics/svg/menu-icon.svg';
 import logo from '../statics/png/foko-logo.png';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = ({ menuToggle, hideLogo }) => {
+    const navigate = useNavigate();
+
     return (
         <div className="fixed top-0 left-0 flex justify-between w-full z-10">
             <div className={`transition-opacity duration-300 ${hideLogo ? "opacity-0" : "opacity-100"}`}>
-                <img src={logo} alt="logo" className="w-[50px] h-[50px] lg:w-[75px] lg:h-[75px] m-[30px] lg:m-[50px]" />
+                <img src={logo} alt="logo" className="w-[50px] h-[50px] lg:w-[75px] lg:h-[75px] m-[30px] lg:m-[50px]" onClick={() => navigate('/')} />
             </div>
             <button onClick={menuToggle} className="cursor-pointer">
                 <img src={menuIcon} alt="menuIcon" className="w-[30px] h-[20px] m-[30px] lg:m-[50px]" />
