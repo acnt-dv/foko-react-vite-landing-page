635d03fa7672086cf7fe70a9e76701139ab8025a
sajedi.dv <d.sajedi@fanap.ir>
2025-06-09 21:08:43 +0330

    logo style fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ({ menuToggle, hideLogo }) => {
 
     return (
         <div className="fixed top-0 left-0 flex justify-between w-full z-10">
-            <div className={`transition-opacity duration-300 ${hideLogo ? "opacity-0" : "opacity-100"}`}>
+            <div className={`transition-opacity duration-300 ${hideLogo ? "opacity-0" : "opacity-100"} cursor-pointer`}>
                 <img src={logo} alt="logo" className="w-[50px] h-[50px] lg:w-[75px] lg:h-[75px] m-[30px] lg:m-[50px]" onClick={() => navigate('/')} />
             </div>
             <button onClick={menuToggle} className="cursor-pointer">
