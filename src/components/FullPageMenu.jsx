2af2151d80d1e34bd8edb7a921acf10287ecdc4c
dv <acnt.dv@gmail.com>
2025-07-26 23:13:13 +0330

    FullPageMenu.jsx improvements by percentage - Desktop mode

--- src/components/FullPageMenu.jsx
+++ src/components/FullPageMenu.jsx
@@ -8,37 +8,39 @@ const FullPageMenu = ({isOpen, onClose}) => {
 
     return (
         <div
-            className={`z-10 fixed inset-0 bg-black opacity-[90%] flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
-            <button onClick={onClose} className="absolute top-[37.5px] right-[30px] lg:top-[72.5px] lg:right-[50px] cursor-pointer">
+            className={`z-10 fixed inset-0 bg-black flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-90 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
+            <button onClick={onClose}
+                    className="absolute top-[37.5px] right-[30px] lg:top-[72.5px] lg:right-[50px] cursor-pointer">
                 <img src={close} alt="close" width='30px' height='30px'/>
             </button>
 
-            <div className="flex flex-col lg:flex-row w-full items-center justify-center">
-                <span className='flex w-1/2 justify-center items-center m-[50px]'>
-                    <img src={logo} alt={'logo'} className='w-[75px] h-[75px] lg:w-[130px] lg:h-[130px] filter brightness-0 invert'/>
+            <div className="flex flex-col lg:flex-row w-full items-center justify-end">
+                <span className='flex justify-end items-center mr-[28.44vw]'>
+                    <img src={logo} alt={'logo'}
+                         className='w-[6.77vw] h-[12.04vh] filter brightness-0 invert'/>
                 </span>
 
                 <nav>
-                    <ul className="flex flex-col lg:flex-row w-1/2 space-y-12 lg:space-y-0 lg:space-x-24 text-center m-[50px]">
-                        <li className="text-white text-20 hover:underline  cursor-pointer">
+                    <ul className="flex flex-col lg:flex-row w-fit space-y-12 lg:space-y-0 text-center">
+                        <li className="text-white text-20 hover:underline cursor-pointer mr-[5.21vw]">
                             <a href="#home" onClick={() => {
                                 onClose();
                                 navigate('/')
                             }}>HOME</a>
                         </li>
-                        <li className="text-white text-20 hover:underline  cursor-pointer">
+                        <li className="text-white text-20 hover:underline cursor-pointer mr-[5.21vw]">
                             <a href="#work" onClick={() => {
                                 onClose();
                                 navigate('/categories')
                             }}>WORK</a>
                         </li>
-                        <li className="text-white text-20 hover:underline  cursor-pointer">
+                        <li className="text-white text-20 hover:underline cursor-pointer mr-[5.21vw]">
                             <a href="#studio" onClick={() => {
                                 onClose();
                                 navigate('/studio')
                             }}>STUDIO</a>
                         </li>
-                        <li className="text-white text-20 hover:underline  cursor-pointer">
+                        <li className="text-white text-20 hover:underline cursor-pointer mr-[7.81vw]">
                             <a href="#contact" onClick={() => {
                                 onClose();
                                 navigate('/contactUs')
