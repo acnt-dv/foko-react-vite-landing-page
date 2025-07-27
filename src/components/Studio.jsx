beeb7d9749ff4c3731163efefeb8f7eabfae9710
dv <acnt.dv@gmail.com>
2025-07-28 00:19:51 +0330

    Studio.jsx improvements by percentage - Desktop mode

--- src/components/Studio.jsx
+++ src/components/Studio.jsx
@@ -5,25 +5,31 @@ const AboutUs = () => {
     return (
         <div className="flex flex-col w-full h-full items-end mt-[18.52vh] ">
             <div className="flex flex-col box-border px-0">
-                <div className="flex flex-col-reverse lg:flex-row mr-[10.42vw]">
+                <div className="flex flex-col-reverse lg:flex-row justify-end mr-[10.42vw]">
 
                     {/* ABOUT US */}
                     <div className="flex flex-col w-[49.48vw] h-[60.19vh] justify-between">
                         <div className='flex w-full justify-end'>
                             <div className='w-fit mr-[2.6vw]'>
                                 <h1 className="font-rubik text-right text-[clamp(10px,1.4vw,30px)]">
-                                    ABOUT <br /> US
+                                    ABOUT <br/> US
                                 </h1>
                             </div>
                             <div className='w-[27.08vw] mr-[10.42vw]'>
                                 <p className="text-[clamp(10px,0.83vw,16px)] text-justify leading-loose tracking-wide
 ">
-                                    FOKO Studio is a Los Angeles-based architecture and interior design firm. Our studio specializes in crafting bespoke, high-end residential and commercial spaces that are as thoughtful as they are visually compelling.
+                                    FOKO Studio is a Los Angeles-based architecture and interior design firm. Our studio
+                                    specializes in crafting bespoke, high-end residential and commercial spaces that are
+                                    as thoughtful as they are visually compelling.
                                 </p>
-                            <br/>
+                                <br/>
                                 <p className="text-[clamp(10px,0.83vw,16px)] text-justify  leading-loose tracking-wide
 ">
-                                    At FOKO Studio, we are deeply committed to our clients' specific needs. From intimate interiors to large scale architectural projects, we combine creative problem-solving with a holistic design approach, navigating the layered complexities of each project. By considering every detail, we design environments that are perfectly tailored to the unique lifestyle and aspirations of our clients.
+                                    At FOKO Studio, we are deeply committed to our clients' specific needs. From
+                                    intimate interiors to large scale architectural projects, we combine creative
+                                    problem-solving with a holistic design approach, navigating the layered complexities
+                                    of each project. By considering every detail, we design environments that are
+                                    perfectly tailored to the unique lifestyle and aspirations of our clients.
                                 </p>
                             </div>
                         </div>
@@ -42,7 +48,12 @@ const AboutUs = () => {
                             <div className='w-[27.08vw] mr-[10.42vw]'>
                                 <p className="text-[clamp(10px,0.83vw,16px)] text-justify  leading-loose tracking-wide
 ">
-                                    At the heart of our practice is the belief that design should create meaningful spaces that enrich the lives of those who inhabit them. We combine critical thinking, technical skill, and a deep respect for context to transform challenges into opportunities and ideas into reality. Whether addressing spatial constraints, aesthetic aspirations, or functional needs, our creative solutions aim to enhance both the experience and impact of each space.
+                                    At the heart of our practice is the belief that design should create meaningful
+                                    spaces that enrich the lives of those who inhabit them. We combine critical
+                                    thinking, technical skill, and a deep respect for context to transform challenges
+                                    into opportunities and ideas into reality. Whether addressing spatial constraints,
+                                    aesthetic aspirations, or functional needs, our creative solutions aim to enhance
+                                    both the experience and impact of each space.
                                 </p>
                             </div>
                         </div>
@@ -58,29 +69,28 @@ const AboutUs = () => {
                     </div>
                 </div>
 
-                {/*<div className="flex flex-col-reverse lg:flex-row w-full pt-[50px] lg:pt-[200px]">*/}
-                {/*    <div className='flex w-full '>*/}
-                {/*        <div className="flex w-full lg:w-3/5">*/}
+                <div className="flex w-full justify-end mt-[18.52vh]">
+                    <div className='w-fit grid justify-end items-end mr-[2.60vw]'>
+                        <p className="font-rubik text-right text-[clamp(10px,1.56vw,30px)]">
+                            THE
+                        </p>
+                        <p className="font-rubik text-right text-[clamp(10px,1.56vw,30px)]">
+                            FOUNDERS
+                        </p>
+                    </div>
+                    <div className='w-[27.08vw] mr-[54.6vw]'>
+                        <p className="text-10 lg:text-16 text-justify">
+                            FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design
+                            professionals with a shared passion for creating inspiring spaces. With diverse backgrounds
+                            in architecture and interior design, Jenny and Pegah bring a unique blend of technical
+                            expertise and creative intuition to every project. Their collaboration fosters a dynamic,
+                            multidisciplinary approach that excels across projects of all scales.
+                        </p>
+                    </div>
+                </div>
 
-                {/*            <div className='w-1/5 grid justify-end items-start max-h-[50px]'>*/}
-                {/*                <p className="text-right text-10 lg:text-30">*/}
-                {/*                    THE*/}
-                {/*                </p>*/}
-                {/*                <p className="text-right text-10 lg:text-30">*/}
-                {/*                    FOUNDERS*/}
-                {/*                </p>*/}
-                {/*            </div>*/}
-                {/*            <div className='w-4/5 px-[30px] lg:pl-[50px] lg:pr-[30px] lg:pr-[50px] lg:pr-[100px] 2lg:pr-[150px]'>*/}
-                {/*                <p className="text-10 lg:text-16 text-justify">*/}
-                {/*                    FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design professionals with a shared passion for creating inspiring spaces. With diverse backgrounds in architecture and interior design, Jenny and Pegah bring a unique blend of technical expertise and creative intuition to every project. Their collaboration fosters a dynamic, multidisciplinary approach that excels across projects of all scales.*/}
-                {/*                </p>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
-                {/*<ProfileCard name='JENNY FONTENOT, RA' />*/}
-                {/*<ProfileCard name='PEGAH KOULAEIAN, LEED AP' />*/}
+                <ProfileCard name='JENNY FONTENOT, RA' />
+                <ProfileCard name='PEGAH KOULAEIAN, LEED AP' />
                 {/*<span className="mb-[200px]" />*/}
             </div>
 
