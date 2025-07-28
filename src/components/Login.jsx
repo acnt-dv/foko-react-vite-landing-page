a7116880a4bef3164708914f70baa9d9e5911722
dv <acnt.dv@gmail.com>
2025-07-29 02:53:04 +0330

    Login bug fixed.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ export const Login = ({loggedIn, setLoggedIn})=> {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <div>
-            <div className="w-[25.64vw] h-[11.85vh]lg:w-[6.77vw] lg:h-[12.04vh] content justify-center items-center">
+            <div className="w-[25.64vw] h-[11.85vh] lg:w-[6.77vw] lg:h-[12.04vh] content justify-center items-center">
                 <img src={logo} alt={'logo'} />
             </div>
         <div className="absolute top-[59.48vh] lg:top-[calc(50%-12.04%vh)]">
