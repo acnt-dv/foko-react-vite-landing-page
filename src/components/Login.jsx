2a571942d36754d11b5e8c8535bb147a51619cf1
dv <acnt.dv@gmail.com>
2025-07-29 03:06:54 +0330

    Login bug fixed and minor improvements.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,17 @@
 import logo from '../statics/png/foko-logo.png';
 
-export const Login = ({loggedIn, setLoggedIn})=> {
+export const Login = ({loggedIn, setLoggedIn}) => {
 
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <div>
-            <div className="w-[25.64vw] h-[11.85vh] lg:w-[6.77vw] lg:h-[12.04vh] content justify-center items-center">
-                <img src={logo} alt={'logo'} />
+            <div className="w-[25.64vw] h-[11.85vh] aspect-square lg:w-[6.77vw] lg:h-[12.04vh] justify-center items-center">
+                <img src={logo} alt={'logo'} className="justify-self-center"/>
             </div>
-        <div className="absolute top-[59.48vh] lg:top-[calc(50%-12.04%vh)]">
-            <button className="text-[clamp(10px, 1.60vw, 16px)] lg:text-[clamp(10px,1.04vw,20px)] leading-none block font-rubik lg:mt-[12.04vh] cursor-pointer"
-            onClick={()=>setLoggedIn(!loggedIn)}
-            >PASSWORD &#62;</button>
-        </div>
+            <div className="flex mt-[3.55vh] lg:mt-[12.04vh]">
+                <button
+                    className="text-[clamp(10px, 1.60vw, 16px)] lg:text-[clamp(10px,1.04vw,20px)] leading-none block font-rubik cursor-pointer"
+                    onClick={() => setLoggedIn(!loggedIn)}
+                >PASSWORD &#62;</button>
             </div>
         </div>
     )
