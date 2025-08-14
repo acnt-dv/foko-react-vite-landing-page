864f4e621fd4d4c925cbd80a866f7ef22ccc5c6d
dv <acnt.dv@gmail.com>
2025-08-14 17:04:15 +0330

    logo icon fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export const Header = ({ menuToggle, hideLogo }) => {
     return (
         <div className="fixed top-0 left-0 flex justify-between w-full z-10">
             <div className={`transition-opacity duration-300 ${hideLogo ? "opacity-0" : "opacity-100"} cursor-pointer`}>
-                <img src={logo} alt="logo" className="w-[50px] h-[50px] lg:w-[75px] lg:h-[75px] m-[30px] lg:m-[50px]" onClick={() => navigate('/')} />
+                <img src={logo} alt="logo" className="w-[12.82vw] h-auto aspect-square lg:w-[3.91vw] lg:h-auto m-[30px] lg:m-[50px]" onClick={() => navigate('/')} />
             </div>
             <button onClick={menuToggle} className="cursor-pointer">
                 <img src={menuIcon} alt="menuIcon" className="w-[30px] h-[20px] m-[30px] lg:m-[50px]" />
