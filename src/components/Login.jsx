508fd124744aa78af9d3f7752a45b26725e142d6
dv <acnt.dv@gmail.com>
2025-09-04 14:34:06 +0330

    some bugs fixed and minor improvements

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,8 +26,9 @@ export const Login = ({loggedIn, setLoggedIn}) => {
                     className="w-[25.64vw] h-[11.85vh] aspect-square lg:w-[6.77vw] lg:h-[12.04vh] justify-center items-center">
                     <img src={logo} alt={'logo'} className="justify-self-center"/>
                 </div>
-                <div className="flex mt-[3.55vh] lg:mt-[12.04vh]">
-                    <div className="relative w-[25.64vw] lg:w-[6.77vw]" ref={containerRef}>
+                <div
+                    className="flex mt-[3.55vh] lg:mt-[12.04vh] w-[25.64vw] lg:w-[6.77vw] justify-self-center justify-center items-center">
+                    <div className="relative" ref={containerRef}>
                         <input
                             ref={inputRef}
                             type="password"
@@ -44,7 +45,7 @@ export const Login = ({loggedIn, setLoggedIn}) => {
                                     }
                                 }
                             }}
-                            className="w-full text-[clamp(10px,1.60vw,16px)] lg:text-[clamp(10px,1.04vw,20px)] leading-none block font-rubik cursor-pointer bg-transparent pr-0 focus:outline-none focus:ring-0"
+                            className="w-full text-[clamp(16px,1.60vw,16px)] lg:text-[clamp(10px,1.04vw,20px)] leading-none block font-rubik cursor-pointer bg-transparent pr-0 focus:outline-none focus:ring-0"
                         />
                         <button
                             onClick={() => {
@@ -55,9 +56,9 @@ export const Login = ({loggedIn, setLoggedIn}) => {
                                     toast.error("Invalid credential");
                                 }
                             }}
-                            className="absolute right-0 top-1/2 -translate-y-1/2 text-lg font-bold text-gray-500 cursor-pointer hover:text-white hover:text-white"
+                            className="absolute right-0 top-1/2 -translate-y-1/2 text-lg font-bold text-gray-500 cursor-pointer"
                         >
-                            <img src={right} alt="Right Image" className='w-[0.42vw] h-[1.48vh]' />
+                            <img src={right} alt="Right Image" className='w-[1vw] lg:w-[0.42vw] lg:h-[1.48vh]'/>
                         </button>
                     </div>
                 </div>
