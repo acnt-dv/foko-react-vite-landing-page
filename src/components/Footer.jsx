ba9b05b19747a27ef3e3e5cbbf3ea4b591773329
dv <acnt.dv@gmail.com>
2025-10-05 14:40:36 +0330

    Refactor Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
+import {INSTAGRAM_LINK} from "../assets/constants.js";
 
 export const Footer = () => {
-    const [showFooter, setShowFooter] = useState(false);
+    const [, setShowFooter] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -15,8 +16,6 @@ export const Footer = () => {
     }, []);
 
     return (
-        // <div className={`fixed bottom-0 left-0 flex justify-between w-full z-50 transition-transform duration-300 ${
-        //     showFooter ? "translate-y-0" : "translate-y-full"}`}>
         <div className={`fixed bottom-0 left-0 flex justify-between w-full z-50 transition-transform duration-300`}>
             <div className="flex w-3/4 justify-start text-[7px] lg:text-10 m-[30px] lg:m-[50px]">
                 <p>@ 2025 FOKO STUDIO. ALL RIGHTS RESERVED.</p>
@@ -24,7 +23,7 @@ export const Footer = () => {
             <div className="flex w-1/4 justify-end text-10 lg:text-16 m-[30px] lg:m-[50px] gap-2 lg:gap-4">
                 {/* <p>LinkedIn</p> */}
                 <p className="cursor-pointer" onClick={() => {
-                    window.open("https://www.instagram.com/foko.studio?igsh=MXE1Y2VmengxNDBsNQ==", "_blank");
+                    window.open(INSTAGRAM_LINK, "_blank");
                 }}>Instagram</p>
             </div>
         </div>
